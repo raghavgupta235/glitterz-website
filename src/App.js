3e13bd5050301543accbdd72efe1a3a17fb474d7
Raghav Gupta <raghav.gupta@cleartrip.com>
2020-12-23 01:54:16 +0530

    Hero Section Integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './css/App.css';
 import Header from './components/Header.js'
 import Footer from './components/Footer.js'
+import HeroSection from './components/HeroSection.js'
 
 function App() {
   return (
     <>
       <Header></Header>
+      <HeroSection></HeroSection>
       <Footer></Footer>
     </>
   );
