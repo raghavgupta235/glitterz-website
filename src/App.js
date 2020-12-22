3a8bf2144f708ccbf8bde24e712219ff7154fb5e
Nakul Aggarwal <you@example.com>
2020-12-23 01:19:24 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './css/App.css';
 import Header from './components/Header.js'
+import Footer from './components/Footer.js'
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Footer></Footer>
     </>
   );
 }
