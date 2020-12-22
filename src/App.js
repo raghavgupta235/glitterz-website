47e724eb8e12ceb85d3434c2507754880829c164
Nakul Aggarwal <you@example.com>
2020-12-23 02:05:13 +0530

    Main added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './css/App.css';
 import Header from './components/Header.js'
 import Footer from './components/Footer.js'
+import Main from './components/Main.js'
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Main></Main>
       <Footer></Footer>
     </>
   );
