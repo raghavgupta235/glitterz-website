87a0da96bbf137f23901a4e0088cc4793994e860
Raghav Gupta <raghav.gupta@cleartrip.com>
2020-12-23 12:59:00 +0530

    Removed Footer Warnings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './css/App.css';
 import Header from './components/Header.js'
-// import Footer from './components/Footer.js'
+import Footer from './components/Footer.js'
 // import Main from './components/Main.js'
 import HeroSection from './components/HeroSection.js'
 
@@ -9,8 +9,8 @@ function App() {
     <>
       <Header></Header>
       <HeroSection></HeroSection>
-      {/* <Main></Main>
-      <Footer></Footer> */}
+      {/* <Main></Main> */}
+      <Footer></Footer>
     </>
   );
 }
